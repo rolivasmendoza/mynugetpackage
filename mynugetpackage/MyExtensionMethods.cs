c6665301c0c2d7cbaa229aba12f79fb6f45dfec7
J Roberto Olivas <contacto@rolivasmendoza.com>
2020-10-01 08:50:30 -0600

    Project's code base line

---
+++ mynugetpackage/MyExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace mynugetpackage
+{
+    public class MyExtensionMethods
+    {
+        public DateTime Today() => System.DateTime.Now;
+
+        public int ThisYear() => System.DateTime.Now.Year;
+    }
+}
